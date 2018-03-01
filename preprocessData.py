744c4a6d53a72ae4768903f0c961a14e9badc20d
Jiang <johnniejyu@gmail.com>
2018-03-01 00:41:11 -0500

    Create preprocessData.py
    Add ToDos

---
+++ preprocessData.py
@@ -0,0 +1,3 @@
+#TODO preprocess iptv data to get user-channel view matrix
+#1, how to transform the start time, end time into view matrix? 
+# Probably slot time and use some threshold to binarize the view history 
